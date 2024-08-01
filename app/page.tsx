8d0def590b49255027c3f776bf0bed61be79230a
ranahwhang <ranahwhang@gmail.com>
2024-08-01 20:37:40 +0900

    Tailwind - Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-2xl w-full">
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
         <div className="flex justify-between items-center">
           <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1">
+            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
               In transit
             </span>
-            <span className="text-4xl font-semibold">Coolblue</span>
+            <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
           </div>
           <div className="size-12 rounded-full bg-orange-400 text-center text-white font-semibold">
             cool
@@ -16,20 +16,20 @@ export default function Home() {
           </div>
         </div>
         <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xl font-medium rounded-full">
+          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xl font-medium rounded-full transition hover:bg-green-500 hover:scale-125">
             Today
           </span>
-          <span>9:30-10:30</span>
+          <span className="dark:text-gray-100">9:30-10:30</span>
         </div>
         <div className="relative">
           <div className="bg-gray-200 w-full h-2 rounded-full absolute" />
           <div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
         </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600">
+        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
           <span>Expected</span>
           <span>Sorting center</span>
           <span>In transit</span>
-          <span className="text-gray-400">Delivered</span>
+          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
         </div>
       </div>
     </main>
